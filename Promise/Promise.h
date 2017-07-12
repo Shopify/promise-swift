aae50607a5d394eb2872117125c0367d7baeb3e8
Sergey Gavrilyuk <sergey.gavryliuk@shopify.com>
2017-07-12 17:38:04 -0400

    Initial Commit

---
+++ Promise/Promise.h
@@ -0,0 +1,19 @@
+//
+//  Promise.h
+//  Promise
+//
+//  Created by Sergey Gavrilyuk on 2017-07-12.
+//  Copyright © 2017 Shopify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Promise.
+FOUNDATION_EXPORT double PromiseVersionNumber;
+
+//! Project version string for Promise.
+FOUNDATION_EXPORT const unsigned char PromiseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Promise/PublicHeader.h>
+
+
