60f1187af3ec668166c0de972c5b3d5c364366f9
Sergey Gavrilyuk <sergey.gavryliuk@shopify.com>
2017-08-02 13:58:12 -0400

    Add schemes for all apple platforms

--- Promise/Promise.h
+++ Promise/Promise.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 Shopify. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Promise.
 FOUNDATION_EXPORT double PromiseVersionNumber;
