a742aa90d95bc17b0f460e4937d9fc81f7f1e8d7
Sergey Gavrilyuk <sergey.gavryliuk@shopify.com>
2017-08-04 17:57:16 -0400

    Update formatting to most profitable: 2 spaces for tab

--- Promise/PromiseNoError.swift
+++ Promise/PromiseNoError.swift
@@ -9,34 +9,34 @@
 import Foundation
 
 extension Promise where E == NoError {
-    
-    @discardableResult
-    public func whenSuccess(_ callback: @escaping (T) -> Void) -> Self {
-        return self.whenComplete { result in
-            switch result {
-            case .success(let v): callback(v)
-            case .error: fatalError()
-            }
-        }
+  
+  @discardableResult
+  public func whenSuccess(_ callback: @escaping (T) -> Void) -> Self {
+    return self.whenComplete { result in
+      switch result {
+      case .success(let v): callback(v)
+      case .error: fatalError()
+      }
     }
-    
-    public func promoteErrors<En: Error>() -> Promise<T, En> {
-        return self.mapError {_ in
-            //will never get there
-            fatalError()
-        }
+  }
+  
+  public func promoteErrors<En: Error>() -> Promise<T, En> {
+    return self.mapError {_ in
+      //will never get there
+      fatalError()
     }
-    
+  }
+  
 }
 
 extension Promise {
-    public func ignoreErrors() -> Promise<T, NoError> {
-        return self.ifErrorThen { _ in
-            return Promise<T, NoError>.never()
-        }
+  public func ignoreErrors() -> Promise<T, NoError> {
+    return self.ifErrorThen { _ in
+      return Promise<T, NoError>.never()
     }
-    
-    public static func never() -> Promise {
-        return Promise {_ in }
-    }
+  }
+  
+  public static func never() -> Promise {
+    return Promise {_ in }
+  }
 }
--- Promise/Result.swift
+++ Promise/Result.swift
@@ -9,16 +9,16 @@
 import Foundation
 
 public enum Result<T, E: Error> {
-    case success(T)
-    case error(E)
+  case success(T)
+  case error(E)
 }
 
 
 extension Result {
-    public func map<V>(_ transform: (T) -> V) -> Result<V, E> {
-        switch self {
-        case .success(let v): return .success(transform(v))
-        case .error(let e): return .error(e)
-        }
+  public func map<V>(_ transform: (T) -> V) -> Result<V, E> {
+    switch self {
+    case .success(let v): return .success(transform(v))
+    case .error(let e): return .error(e)
     }
+  }
 }
