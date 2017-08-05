b78a231c28b19c55979567dd7a4f6ba63c99aa92
Sergey Gavrilyuk <sergey.gavryliuk@shopify.com>
2017-08-05 13:27:37 -0400

    Docs provided in comments for Promise Core

--- Promise/PromiseCore.swift
+++ Promise/PromiseCore.swift
@@ -35,23 +35,66 @@ fileprivate enum PromiseState<T, E: Error> {
   }
 }
 
+
+/// Manual promise resolver.
+/// 
+/// You don't create an instance of resolver mannually. `Promise` provides 
+/// instance of resolver in async operation wrapper closure so caller can manually resolve or reject 
+/// `Promise` when async operation is finished.
+/// Also, `PromiseResolver` bears cancellation action, so you async operation can inject custom 
+/// cancellation action into resolver using `onCancel` property
+
 public class PromiseResolver<T, E: Error> {
+  
+  
+  /// Cancellation action. Async operation can provide custom action to be performed
+  /// when `Promise` is cancelled using this property.
   public var onCancel: PromiseCancelFunction?
+  
+  
+  /// Promise `complete` handler.
+  ///
+  /// Combines both resolution AND rejection values in common `Result` type.
+  /// `resolve(with:)` and `reject(with:)` use this property to forward result to observers.
+  
   private(set) public var complete: PromiseCallback<T, E>
   
   fileprivate init(complete: @escaping PromiseCallback<T, E>) {
     self.complete = complete
   }
   
+  
+  /// Resolves `Promise`.
+  ///
+  /// Calling `resolve` will synchronously forward result into `Promise` observers, including 
+  /// chained `Promise`s, unless current `Promise` has already been cancelled, in which case this call
+  /// does nothing.
+  ///
+  /// - Parameter value: success value for promise to be resolved with.
+  
   public func resolve(with value: T) {
     complete(.success(value))
   }
+  
+  
+  /// Rejects `Promise`
+  ///
+  /// Calling `reject` will synchronously forward result into `Promise` observers,
+  /// unless current `Promise` has already been cancelled, in which case this this call.
+  /// Chained `Promise`s will not be created as errors are not propagated into `Promise` chain.
+  ///
+  /// - Parameter error: error value for promise to be rejected with
   
   public func reject(with error: E) {
     complete(.error(error))
   }
   
 }
+
+
+/// Promise 
+///
+/// Represnts value that may be available in future.
 
 final public class Promise<T, E: Error> {
   
@@ -69,18 +112,56 @@ final public class Promise<T, E: Error> {
     }
   }
   
+  
+  /// Creates resolved `Promise` with success result of given value
+  ///
+  /// - Parameter value: value that is immediately available.
+  
   public convenience init(value: T) {
     self.init(state: .complete(.success(value)))
   }
   
+  
+  /// Creates rejected `Promise` with error result of given value
+  ///
+  /// - Parameter error: error that is emmediately available.
   public convenience init(error: E) {
     self.init(state: .complete(.error(error)))
   }
+  
+  
+  /// Creates `Promise` wrapping asynchronous action
+  ///
+  /// Returned `Promise` instance is in pending state. Upon first call to `whenComplete`
+  /// `Promise` will execute start function, which supposed to start wrapping asynchronous operation
+  /// Asynchronous operation can report when it's finished — either successfully or not — using `resolver`
+  /// given as parameter to start function.
+  /// `resolver` also retains cancellation action, which start function wrapping asynchronous operation
+  /// can define using `onCancel` property of `resolver`
+  /// ```
+  /// let p = Promise<Int, NoError> { resolver in
+  ///   let operation = startAsyncOperation() { result in
+  ///     resolver.resolve(result)
+  ///   }
+  ///   resolver.onCancel = { operation.cancel() }
+  /// }
+  /// ```
+  ///
+  /// - Parameter start: start function wrapping asynchronous action
   
   public convenience init(_ start: @escaping PromiseStartFunction<T, E>) {
     self.init(state: .pending(start, []))
   }
   
+  
+  /// Adds observer to the `Promise`
+  /// 
+  /// Adds callback to observe result of the `Promise` (either resolution or rejection).
+  /// Callback given may be called synchronously upon calling `whenComplete` (if the result is already available).
+  /// May be called on any thread, depending on which thread asynchronous operation notified about availbale result.
+  ///
+  /// - Parameter callback: function to be called when result is available.
+  /// - Returns: self instance of `Promise` that can be ignored. Used for call chaning purposes.
   @discardableResult
   public func whenComplete(callback: @escaping PromiseCallback<T, E>) -> Self {
     state.modify {
@@ -122,6 +203,15 @@ final public class Promise<T, E: Error> {
       callback(result)
     }
   }
+  
+  
+  /// Cancels executing `Promise`
+  /// 
+  /// If the `Promise` is alreadt canceled — does nothing.
+  /// If the `Promise` is currently executing — calls cancellation action (providing via `onCancel` property of resolver)
+  /// and ignores any subsequent attempts to resolve or reject `resolver` from asynchronous operation.
+  /// `whenComplete` is guaranteed to never be called after `cancel` was called.
+  /// Any `Promise`s chained with this one via `then` will never be created.
   
   public func cancel() {
     state.modify {
