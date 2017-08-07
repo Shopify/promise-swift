3904ca51f564b27ea9dd5e6b902aca9435133987
Sergey Gavrilyuk <sergey.gavryliuk@shopify.com>
2017-08-07 13:58:39 -0400

    Add documentation to Promise handy extensions

--- Promise/PromiseExtension.swift
+++ Promise/PromiseExtension.swift
@@ -10,6 +10,17 @@ import Foundation
 
 extension Promise {
   
+  
+  /// Dispatch `Promise` completion event on a queue.
+  /// 
+  /// Creates new `Promise` which is guaranteed to call on observers with completion event
+  /// (either rejection or resolution) on a given GCD queue.
+  /// **Note**: this does not modify current `Promise`, only returned instance is guaranteed to 
+  /// to use queue for scheduling.
+  ///
+  /// - Parameter queue: queue to schedule observers callback on
+  /// - Returns: new `Promise` instance.
+  
   public func completeOn(queue: DispatchQueue) -> Promise {
     return Promise { resolver in
       self.whenComplete { result in
@@ -17,6 +28,17 @@ extension Promise {
       }
     }
   }
+  
+  
+  /// Injects a delay in async computation process.
+  ///
+  /// Creates new `Promise` which adds a given delay (using `disatchAfter` on a given queue)
+  /// before notifying observers about completion event (both resolution and rejection).
+  ///
+  /// - Parameters:
+  ///   - delay: delay to add before notifying observers
+  ///   - queue: queue to schedule `dispatchAfter` on
+  /// - Returns: new `Promise` instance.
   
   public func delayed(for delay: DispatchTimeInterval, on queue: DispatchQueue = DispatchQueue.main) -> Promise {
     return Promise { resolver in
