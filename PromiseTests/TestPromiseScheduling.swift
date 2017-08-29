020eb28d193a4911b81411b5ad0ac939d5099707
Sergey Gavrilyuk <sergey.gavryliuk@shopify.com>
2017-08-29 17:10:35 -0400

    Add tests for `startOn`and `completeOn`

--- PromiseTests/TestPromiseScheduling.swift
+++ PromiseTests/TestPromiseScheduling.swift
@@ -31,8 +31,6 @@ class TestPromiseScheduling: XCTestCase {
     }
     
     func testStartOn() {
-        let p: Promise<Int, TestError> = makeAsyncPromise(result: .success(42), delay: .milliseconds(200))
-        
         var onExecuted = false
         let observableQueue = TestQueue(async: { work in
             onExecuted = true
@@ -40,11 +38,51 @@ class TestPromiseScheduling: XCTestCase {
         }, asyncAfter: { deadline, work in
             DispatchQueue.main.asyncAfter(deadline: deadline, execute: work)
         })
+
+        let p: Promise<Int, NoError> = Promise { resolver in
+            // see if onStarted action has been already been executed.
+            XCTAssertTrue(onExecuted)
+            resolver.resolve(with: 42)
+        }
         
         let exp = expectation(description: "")
         p.startOn(queue: observableQueue).whenComplete { _ in exp.fulfill() }
         waitForExpectations(timeout: 1.0)
-        XCTAssertTrue(onExecuted)
+    }
+    
+    func testCompleteOn() {
+
+        enum Resolution { case resolved, rejected }
+        
+        func testCompleteOn(with resolution: Resolution) {
+            var onExecuted = false
+            let observableQueue = TestQueue(async: { work in
+                onExecuted = true
+                DispatchQueue.main.async(execute: work)
+            }, asyncAfter: { deadline, work in
+                DispatchQueue.main.asyncAfter(deadline: deadline, execute: work)
+            })
+            
+            let p: Promise<Int, TestError> = Promise { resolver in
+                // see if onCompleted action only executed _after_ this promise completed
+                XCTAssertFalse(onExecuted)
+                switch resolution {
+                case .resolved: resolver.resolve(with: 42)
+                case .rejected: resolver.reject(with: .error1)
+                }
+                
+                XCTAssertTrue(onExecuted)
+            }
+            
+            let exp = expectation(description: "")
+            p.completeOn(queue: observableQueue).whenComplete { _ in exp.fulfill() }
+            waitForExpectations(timeout: 1.0)
+            
+        }
+        
+        testCompleteOn(with: .resolved)
+        testCompleteOn(with: .rejected)
+        
     }
     
 }
